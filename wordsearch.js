19513c81e1bd089ec70f9f65144caa1a8f3c5e36
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-08 23:14:32 +0000

    finished horizontal and backwards search, also finished reverse search for word

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+const wordSearch = (letters, word) => {
+
+  let reverseWord = word.split("").reverse().join("");
+
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+      if (l.includes(word)) {
+          return true
+      } else if (l.includes(reverseWord)) {
+          return true
+      }
+  }
+
+
+  let gridFlip = function (matrix) {
+
+      let outerArray = []
+
+      for (let x = 0; x < matrix.length; x++) {
+          for (let y = 0; y < matrix[x].length; y++) {
+              if (x === 0) {
+                  outerArray[y] = [matrix[x][y]]
+              } else {
+                  outerArray[y].push(matrix[x][y])
+              }
+          }
+      }
+      return outerArray
+  }
+
+  let flip = gridFlip(letters)
+
+  const verticalJoin = flip.map(ls => ls.join(''))
+  for (l of verticalJoin) {
+      if (l.includes(word)) {
+          return true
+      } else if (l.includes(reverseWord)) {
+          return true
+      }
+  }
+return false
 }
+
+
 
 module.exports = wordSearch
